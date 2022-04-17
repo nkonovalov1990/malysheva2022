d065bb213301e106404ee2ce7b23607bf2eb701f
nkonovalov1990 <n.konovalov1990@gmail.com>
2022-04-17 19:06:20 +0500

    1

--- data.js
+++ data.js
@@ -1,22 +1,22 @@
 const PLANS = [
   {
-    title: 'Исходны план',
-    map: 'current.png',
-    // legend: 'legend-old.png',
+    title: 'Новый план c БРТ',
+    map: 'with-BRT.png',
+    // legend: 'functional-zones.legend.png',
     pinned: true,
-    old: true,
     default: true
   },
   {
     title: 'Новый план без БРТ',
     map: 'without-BRT.png',
-    // legend: 'legend-new.png',
+    // legend: 'functional-zones.legend.png',
     pinned: true
   },
   {
-    title: 'Новый план с БРТ',
-    map: 'with-BRT',
-    // legend: 'legend-new.png',
-    pinned: true
+    title: 'Исходный план',
+    map: 'current.jpg',
+    // legend: 'old.legend.jpg',
+    pinned: true,
+    old: true
   }
 ];
